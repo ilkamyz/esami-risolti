07dedfd68a89c2f8f2aa13220afc3bb58c1925bb
Camillo Veneziale <camilloveneziale@gmail.com>
2025-11-12 15:25:54 +0100

    Voti Risolto

--- voti/main.py
+++ voti/main.py
@@ -1,33 +1,41 @@
-﻿# Write your solution here, DO NOT START A NEW PROJECT
-# ATTENTION: if you create a new project, your exam paper will not be collected
-#            and you will be obliged to come in the subsequent exam session
-#
-# ATTENTION: on Win 10 (Italian keyboard) characters like [ ] { } have to be
-#            created using ALTgr+è (e.g. for [ ) and NOT CTRL-ALT-è
-#
-# ATTENTION: on macOS you have to use CTRL-C and CTRL-V inside the virtual
-#            machine and NOT command-C command-V
-#
-# if your keyboard is broken you can do copy/paste also with mouse
-# and you can copy special characters like [ ] { } < > here
-#
-# Scrivete qui la vostra soluzione, NON CREATE UN NUOVO PROGETTO
-# ATTENZIONE: se create un nuovo progetto il vostro compito non sara'
-#             raccolto correttamente e dovrete tornare all'appello successivo
-#
-# ATTENZIONE: su Win 10 (tastiera italiana) i caratteri speciali (es. { ) vanno
-#             scritti ad esempio con ALTgr+è (caso di [ ) e NON CTRL-ALT-è
-#
-# ATTENZIONE: su macOS vanno usati CRTL-C e CTRL-V per il copia incolla
-#                       nella macchina virtuale e NON command-C command-V
-#
-# se la vostra tastiera è guasta potete fare copia/incolla anche con il mouse
-# e per i caratteri speciali potete copiare da questi caratteri  [  ]  {  }  <  >
-# print(string.punctuation)
-## ! " # $ % & ' ( ) * + , - . / : ; < = > ? @ [ \ ] ^ _ ` { | } ~
+﻿import csv
+from statistics import mean
+def readAppello(filename: str) -> dict:
+    idx1 = 0
+    dizionario = {}
+    with open(filename,'r') as f:
+        dictreader = csv.DictReader(f)
+        for line in dictreader:
+            dizionario[idx1] = tuple(line.values())
+            idx1 += 1
+    return dizionario
 
+def joinAppelli(appelli: list[dict]) -> dict:
+    'return a 0 indexed dictionary with all the appelli joined'
+    idx = 0
+    return_dict = {}
+    for appello in appelli:
+        for val in appello.values():
+            return_dict[idx] = val
+            idx += 1
+    return return_dict
 
-print(open('primo_appello.csv', 'r').read())
-print()
-print(open('secondo_appello.csv', 'r').read())
-print()
+primo_appello = readAppello('primo_appello.csv')
+secondo_appello = readAppello('secondo_appello.csv')
+joined_appelli = joinAppelli([primo_appello, secondo_appello])
+
+sorted_keys = sorted(joined_appelli.keys(),key = lambda x: float(joined_appelli[x][-1]), reverse= True)
+
+with open('sessione.csv', 'w') as f:
+
+    f.write('Nome,Cognome,Voto,Anno_di_Nascita,Media_Voti\n')
+    for i in sorted_keys:
+        nome, cognome, voto, anno, media_voti = joined_appelli[i]
+        f.write(f'{nome},{cognome},{anno},{voto},{media_voti}\n')
+    f.close()
+
+differenze = []
+for key in sorted_keys[:5]:
+    _, _, voto, _, media_voti = joined_appelli[key]
+    differenze.append(float(voto) - float(media_voti))
+print(f'La media delle differenze tra voto e media nei primi 5 studenti è {mean(differenze):.2f}')
