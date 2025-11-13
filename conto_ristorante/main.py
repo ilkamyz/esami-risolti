14c3069d77d907b571672dac83821c9e9e4eeb06
Camillo Veneziale <camilloveneziale@gmail.com>
2025-11-14 00:11:18 +0100

    conto_ristorante risolto
    Molto facile, max 25 righe di codice

---
+++ conto_ristorante/main.py
@@ -0,0 +1,23 @@
+﻿def main():
+    ordine = {}
+    with open ('ordine.txt', 'r') as f:
+        for line in f:
+            idx, quantity = line.strip().split(',')
+            ordine[idx] = [int(quantity)]
+    with open('menu.txt', 'r') as f:
+        for line in f:
+            (idx, nome, prezzo, iva) = line.strip().split(',')
+            iva, prezzo = float(iva), float(prezzo)
+            if idx in ordine.keys():
+                ordine[idx] += [nome, prezzo, iva]
+    print('RICEVUTA')
+    totale, totaleIva = 0, 0
+    for _, (quantity, nome, prezzo, iva) in ordine.items():
+        print(f' {quantity}  {nome:<22} {quantity * prezzo:>5.2f} IVA {iva:>4.2f}%')
+        totale += quantity * prezzo
+        totaleIva += quantity * prezzo * iva/100
+    print(f'Totale: {totale:.2f}€')
+    print(f'Di cui IVA: {totaleIva:.2f}€')
+
+if __name__ == '__main__':
+    main()
